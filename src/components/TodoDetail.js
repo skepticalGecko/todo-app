84b5c91593b2d3300a3cbb89d02f8e5eaab9d5e1
Matthew Devane <scrdnn@gmail.com>
2022-03-22 15:00:20 -0400

    Fix tbody warning. Add row color for complete

--- src/components/TodoDetail.js
+++ src/components/TodoDetail.js
@@ -4,15 +4,13 @@ export default function TodoDetail(props){
     let {id} = useParams()
     let labels = ["Id", "Title", "Status", "Username", "Name", "Email", "Website", "City", "Company Name", "Phone"] 
     return <div><table className="table">
+            <tbody>
             {
-                labels.map((label)=>{ return <tr key={label}>
-                        <td>{label}</td>
-                        {props.getTableDataElementForLabel(label, parseInt(id), false)}
-                    </tr>
+                labels.map((label)=>{ return <tr key={label}><td>{label}</td><td>{props.getTableDataElementForLabel(label, parseInt(id), false)}</td></tr>
                 })
             }
+            </tbody>
             </table>
-
             <Link to={"/"}>Go Back</Link>
             </div>
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 export default function TodoItem(props){
-    return <tr>
+
+    return <tr className={props.todoItem.completed ? "table-success" : ""}>
         {props.headers && props.headers.length && props.headers.length > 0 &&
             props.headers.map(header => {
                 return <td key={header}>
